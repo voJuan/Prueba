8f5d81a244df23612212d7e8c46bff0fc5d5f5c7
Natan Matias Rodriguez <neitanrodriguez@hotmail.com>
2024-07-17 19:57:45 -0300

    Nueva Pantalla inicio

---
+++ tp_poo/PantallaInicioUI/pantallainicioui.h
@@ -0,0 +1,15 @@
+#ifndef PANTALLAINICIOUI_H
+#define PANTALLAINICIOUI_H
+
+#include <QWidget>
+
+class PantallaInicioUI : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit PantallaInicioUI(QWidget *parent = nullptr);
+
+signals:
+};
+
+#endif // PANTALLAINICIOUI_H
